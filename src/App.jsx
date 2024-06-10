66a7dcb3357b7db13e4c3ae4b3e224835d6557bc
henrique-silva-22 almeida <bob792020@gmail.com>
2024-06-10 01:51:46 +0000

    instalação

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
 
-export default function App() {
+import {theme} from "./styles/theme.js"
+import {Home} from "./pages/Home"
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element:  <Home />
+  }
+])
+
+export function App() {
   return (
-    <div>App</div>
+    <ThemeProvider theme={theme}> 
+          <RouterProvider router={router} />
+
+    </ThemeProvider>
   )
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+    homeBackground: "#131518",
+    carGradient: "radial-gradient(96.96% 98.96 at 50% 0%, #232A34 0%, #1B1E27 100%)",
+    textColor: "#969FAD",
+    submitButtonColor: "#FC7614",
+    white: "#FFF",
+    feedbackButtonColor: "#262E38",
+    deedbackButtonText: "#7C8798"
+
+}
